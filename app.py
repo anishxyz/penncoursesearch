a9656fd2935a3b70b4fdeaa492d638ae4e79c479
anishagrawal <anishagrawal2003@gmail.com>
2023-04-03 05:03:03 -0500

    embed file 1

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import os
+
 import queryengine
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from queryengine import query_response
 
@@ -10,9 +12,13 @@ CORS(app)
 # df = queryengine.load_df()
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 
 @app.route('/search', methods=['GET'])
@@ -20,9 +26,9 @@ async def search():
     search_term = request.args.get('q', '')
     # Implement your search logic here, using search_term
     # For example, you can filter a list of courses based on the search term:
-    results = await query_response(search_term, df)
+    # results = await query_response(search_term, df)
     print("got it!")
-    return jsonify(results)
+    return jsonify("hello")
 
 
 if __name__ == '__main__':
